8f960948698c342de8371e19252680539d27b1d2
sjoleee <cent7425@gmail.com>
2022-04-24 18:40:24 +0900

    product create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import "./App.css";
+import Date from "./data.js";
 
 function App() {
+  const [appearl, setAppearl] = useState(Date);
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">GSSHOP</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="background">
+        <h1>hello, world</h1>
+        <p>welcome to sjoleee's GSHOP</p>
+      </div>
+      <div className="container">
+        <div className="row">
+          {appearl.map((params, idx) => {
+            return <Product params={params} idx={idx} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Product(props) {
+  return (
+    <div className="col-md-4">
+      <img src={props.params.src}></img>
+      <h4>{props.params.title}</h4>
+      <p>{props.params.content}</p>
+      <p>{props.params.price}</p>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    id: 0,
+    title: "오버사이즈 실켓 하프 슬리브 티셔츠",
+    content: "상조어패럴",
+    price: "48,000원",
+    src: "https://image.msscdn.net/images/goods_img/20220419/2499333/2499333_1_500.jpg",
+  },
+
+  {
+    id: 1,
+    title: "위빙 카라 니트",
+    content: "상조어패럴",
+    price: "68,000원",
+    src: "https://image.msscdn.net/images/goods_img/20220419/2499224/2499224_2_500.jpg",
+  },
+
+  {
+    id: 2,
+    title: "오버사이즈 스트라이프 셔츠",
+    content: "상조어패럴",
+    price: "78,000원",
+    src: "https://image.msscdn.net/images/goods_img/20220419/2499306/2499306_1_500.jpg",
+  },
+];
