c1c0f94d0746c5de16f7ffa84e34472e934a50a5
sjoleee <cent7425@gmail.com>
2022-05-08 15:43:41 +0900

    modal onoff

--- src/Detail.js
+++ src/Detail.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import styled from "styled-components";
 import "./Detail.scss";
@@ -9,6 +9,13 @@ const TitleBox = styled.div`
 `;
 
 function Detail(props) {
+  let [alert, setAlert] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+  });
   let { id } = useParams();
   let history = useHistory();
   let selectedProduct = props.appearl.find((i) => {
@@ -18,7 +25,9 @@ function Detail(props) {
   return (
     <div className="container">
       <TitleBox className="red">Detail</TitleBox>
-      <div className="alert">재고가 얼마 남지 않았습니다.</div>
+      {alert === true ? (
+        <div className="alert">재고가 얼마 남지 않았습니다.</div>
+      ) : null}
       <div className="row">
         <div className="col-md-6">
           <img src={selectedProduct.src} width="100%" alt="" />
